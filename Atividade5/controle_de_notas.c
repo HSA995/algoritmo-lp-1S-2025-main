9a008db1fdd6a3950e0ec21e6fbaada6f71a6cf0
Henrique S0uto <henriquesouto995@gmail.com>
2025-04-18 18:21:18 -0300

    notas

--- Atividade5/controle_de_notas.c
+++ Atividade5/controle_de_notas.c
@@ -1,49 +1,71 @@
 #include <stdio.h>
 
-// int mediaVetor(float vet[], int tam){
-//     float media, soma = 0;
-//     int i;
-
-//     for(i =0; i<tam; i++){
-//     soma = soma + vet[i];
-//   }
-//     media = (float) soma/tam;
-
-//     return media;
-// }
-
-// int main(){
-//     int alunos;
-//     printf("Defina a quantidade de alunos: \n");
-//     scanf("%d", &alunos);
-
-//     float vetor[alunos];
-//     printf("digite a nota:\n", vetor[0]);
-//     scanf("%.2f", &vetor[0]);
-
-//     float resultado;
-
-//     resultado = mediaVetor(vetor, alunos);
-//     printf("A media dos vetores e de %.2f\n", resultado);
-// }
-
 int main (int argc, char* argv[]){
 
-    int alunos;
+    int qtdAlunos = 0;
 
-    printf("defina a quantidade de alunos:\n");
-    scanf("%d", &alunos);
+    printf("Informe a quantidade de alunos: \n");
+    scanf("%d", &qtdAlunos);
 
-    float notas[alunos];
+    float notas[100];
 
-    for(int i = 0; i < alunos; i++){
-        printf("\ndigite a nota do aluno %d:", i);
-        scanf("%f", &notas[i]);
+    for(int i = 0; i < qtdAlunos; i++){
+        do {
+            printf("\nDigite a nota do aluno %d (entre 0.0 e 10.0): ", i);
+            scanf("%f", &notas[i]);
+    
+            if(notas[i] < 0.0 || notas[i] > 10.0) {
+                printf("Nota invalida! Tente novamente.\n");
+            }
+    
+        } while(notas[i] < 0.0 || notas[i] > 10.0);
     }
+    
 
-        for(int i = 0; i < alunos; i++){
-        printf("\nNota do aluno %.2f:", i, notas[i]);
+for(int i = 0; i < qtdAlunos; i++){
+    printf("\nNota do aluno %d: %.2f ", i, notas[i]);
+}
+
+    float soma = 0;
+    for(int i = 0; i < qtdAlunos; i++) {
+    soma += notas[i];
+    } 
+
+    float media = soma / qtdAlunos;
+    printf("\nMedia da turma: %.2f\n", media);
+
+    float maior = notas[0];
+    float menor = notas[0];
+    for(int i = 1; i < qtdAlunos; i++) {
+    if(notas[i] > maior) {
+        maior = notas[i];
     }
+    if(notas[i] < menor) {
+        menor = notas[i];
+    }
+    }
+    printf("Maior nota: %.2f\n", maior);
+    printf("Menor nota: %.2f\n", menor);
+
+    int aprovados = 0;
+    for(int i = 0; i < qtdAlunos; i++) {
+    if(notas[i] >= 6.0) {
+        aprovados++;
+    }
+    }
+    printf("Alunos aprovados (nota >= 6.0): %d\n", aprovados);
+
+    int k = -1;
+do {
+    printf("Informe a posicao k do aluno (0 ate %d): ", qtdAlunos - 1);
+    scanf("%d", &k);
+
+    if (k < 0 || k >= qtdAlunos) {
+        printf("Posicao invalida! Tente novamente.\n");
+    }
+} while (k < 0 || k >= qtdAlunos);
+
+printf("Nota do aluno na posicao %d: %.2f\n", k, notas[k]);
 
     return 0;
 
